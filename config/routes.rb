8e8090123935c96f7cbb41c7db43da8d46edc1ec
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-03-28 11:53:59 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # HTTPメソッド 'URIパターン', to: 'コントローラー名#アクション名'
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
   post 'posts', to: 'posts#create'
 end
