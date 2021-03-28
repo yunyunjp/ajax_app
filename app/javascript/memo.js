7a4dc8fbdd219d0156527f5f1ace53ac4b854ffd
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-03-28 16:36:08 +0900

    Revert "JSでレスポンスを処理する"
    This reverts commit fa33b8ece1e4e56519d3143e603ec1e18752b1ea.

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,17 +1,3 @@
-const buildHTML = (XHR) => {
-  const item = XHR.response.post;
-  const html = `
-    <div class="post">
-      <div class="post-date">
-        投稿日時：${item.created_at}
-      </div>
-      <div class="post-content">
-        ${item.content}
-      </div>
-    </div>`;
-  return html
-}
-
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
@@ -22,16 +8,6 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
-    XHR.onload = ()  => {
-      if (XHR.status != 200) {
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        return null;
-      }
-      const list = document.getElementById("list");
-      const formText = document.getElementById("content");
-      list.insertAdjacentHTML("afterend", buildHTML(XHR));
-      formText.value = "";  
-    };
   });
 }
 
