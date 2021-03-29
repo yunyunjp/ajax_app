eeaefc5b63c99eee78196b987a95bc0e1906c869
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-03-29 10:54:01 +0900

    コンフリクトの削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,22 +8,15 @@ class PostsController < ApplicationController
   # end  
 
   def create
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     
     post = Post.create(content: params[:content])
     render json:{ post: post }
   end
 
 end
-=======
-=======
->>>>>>> parent of 11a1cfd (コントローラーにリクエストを送信する)
+
     Post.create(content: params[:content])
     redirect to action: index
   end  
 end
-<<<<<<< HEAD
->>>>>>> parent of 11a1cfd (コントローラーにリクエストを送信する)
-=======
->>>>>>> parent of 11a1cfd (コントローラーにリクエストを送信する)
